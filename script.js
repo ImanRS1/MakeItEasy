d86835f5a6c487844710011d198e7ea215bf16f2
ImanRS1 <omid-omega@mail.com>
2020-11-18 22:11:01 +0100

    slider init, added old pics

--- script.js
+++ script.js
@@ -3,3 +3,36 @@ function phaseHoverOn(){
 
 function phaseHoverOff(){
 }
+
+
+startAutoSlide();
+var myVar;
+
+function startAutoSlide(){
+  myVar = setInterval(function(){ plusSlides(1); }, 9000);
+}
+
+var bSlideIndex = 1;
+bShowSlides(bSlideIndex);
+
+function plusSlides(n){
+  bShowSlides(bSlideIndex += n);
+}
+function currentSlide(n){
+  bShowSlides(bSlideIndex = n);
+}
+function bShowSlides(n){
+  var i;
+  var slides = document.getElementsByClassName("bSlideImages");
+  if(n > slides.length){
+    bSlideIndex = 1;
+  }
+  if(n < 1){
+    bSlideIndex = slides.length;
+  }
+  for(i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[bSlideIndex - 1].style.display = "block";
+}
